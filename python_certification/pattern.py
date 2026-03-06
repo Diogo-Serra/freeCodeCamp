34e9d1ba922b9023b62add2d4482477c54b8623c
Diogo-Serra <di-code@outlook.com>
2026-03-06 18:13:50 +0000

    updates

---
+++ python_certification/pattern.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def number_pattern(n):
+    list1 = []
+    for i in range(1, n + 1):
+        list1.append(i)
+    for i in list1:
+        print(i)
+
+
+number_pattern(5)
